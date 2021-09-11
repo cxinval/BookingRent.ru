76fb58e59f76ae7d8ff87df8ce0cc1939c077ddc
cxinval <cxinval7@mail.ru>
2021-09-11 14:53:34 +0300

    update

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    const swiper = new Swiper('#swiper_min', {
+        // Optional parameters
+        slidesPerView: 'auto',
+        direction: 'horizontal',
+        loop: true,
+
+
+
+
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
